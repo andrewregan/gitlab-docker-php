e6ffc6fa2f6e81122436a4b1a9613e6d6a8aa9c2
Andrew Regan <a.jamesregan@gmail.com>
2016-05-29 12:22:51 -0400

    Use gulp html5 linter instead of w3m

--- setup.sh
+++ setup.sh
@@ -16,15 +16,13 @@
 # install dependencies tools
 apt-get install -yqq git php7.0-mysql npm nodejs
 
-# install css and js testings tools
+# install css js and html testings tools
 npm install -g csslint
 npm install -g jslint
+npm install -g html5-lint
+npm install -g gulp-cli
+npm install -g gulp-html5-lint
 ln -s "$(which nodejs)" /usr/bin/node
-
-# install html5-tidy
-curl http://binaries.html-tidy.org/binaries/tidy-5.2.0/tidy-5.2.0-64bit.deb -o tidy.deb
-dpkg -i tidy.deb
-rm tidy.deb
 
 # install php composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
