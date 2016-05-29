ca9334336f23e4f5c8860f6d114630c285be5256
Andrew Regan <a.jamesregan@gmail.com>
2016-05-29 10:19:03 -0400

    Remove code to install mysql

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # install password generator
-apt-get install -y pwgen
+#apt-get install -y pwgen
 
 # generate a password for the MySQL server
-PASSWORD=`pwgen 10 1`
-PASSWORD='docker'
-echo "MySQL Password: $PASSWORD"
+#PASSWORD=`pwgen 10 1`
+#PASSWORD='docker'
+#echo "MySQL Password: $PASSWORD"
 
 # set mysql password when prompted
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+#debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+#debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+#apt-get install -y mysql-server mysql-client
 
 # install testing tools
-apt-get install -y git php7.0-mysql mysql-server mysql-client npm nodejs
+apt-get install -y git php7.0-mysql npm nodejs
 npm install -g csslint
 ln -s "$(which nodejs)" /usr/bin/node
 
